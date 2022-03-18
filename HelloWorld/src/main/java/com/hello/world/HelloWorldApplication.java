4e484014f7101cad6ba241f1dbac1b592d563594
kickic-roong <101728345+kickic-roong@users.noreply.github.com>
2022-03-18 22:54:12 +0700

    Adding print message

--- HelloWorld/src/main/java/com/hello/world/HelloWorldApplication.java
+++ HelloWorld/src/main/java/com/hello/world/HelloWorldApplication.java
@@ -8,6 +8,9 @@ public class HelloWorldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
+		
+		
+		System.out.println("Hello World. I can commit code. Hooray!!!");
 	}
 
 }
